13594b6de8e8e78e26466ccdeeb30f23425ea97d
elenayran <36763284+elenayran@users.noreply.github.com>
2022-01-06 00:55:03 +0300

    player controller mekanikleri yapılma aşaması

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float runningSpeed;
+    private float _currentRunningSpeed;
+    public float limitX;
+    //public float xMin, xMax;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _currentRunningSpeed = runningSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float newX = 0;
+        float touchDelta = 0;
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + _currentRunningSpeed * Time.deltaTime);
+        transform.position = newPosition;
+        //Mathf.Clamp(transform.position.x, xMin, xMax);
+
     }
 }
