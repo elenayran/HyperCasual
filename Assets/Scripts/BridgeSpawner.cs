6a547d58eb757d331250e14dff5f0c706c75e4c8
elenayran <36763284+elenayran@users.noreply.github.com>
2022-03-18 02:32:46 +0300

    tuzak sistemi

--- Assets/Scripts/BridgeSpawner.cs
+++ Assets/Scripts/BridgeSpawner.cs
@@ -14,6 +14,10 @@ public class BridgeSpawner : MonoBehaviour
         float distance = direction.magnitude;
         direction = direction.normalized;
         hiddenPlatform.transform.forward = direction;
+        hiddenPlatform.size = new Vector3(hiddenPlatform.size.x, hiddenPlatform.size.y, distance);
+
+
+        hiddenPlatform.transform.position = startReference.transform.position + (direction * distance / 2) + (new Vector3(0, -direction.z, direction.y)* hiddenPlatform.size.y / 2);
     }
 
    
